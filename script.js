a08a85d763e0bf9af45f83c3617fd320bd03c184
nerminDonlag <nermin.donlag@gmail.com>
2022-03-05 00:37:03 +0100

    Add modal styling

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+document.querySelector('.theme-cont > button').addEventListener('click', () => {
+  changeColor();
+});
+
 function changeColor() {
   const root = document.documentElement;
   const newColor = root.className === 'dark' ? 'bright' : 'dark';
   root.className = newColor;
 }
 
-document.querySelector('.theme-cont > button').addEventListener('click', () => {
-  changeColor();
+document.querySelector('.add-book-cont > button').addEventListener('click', () => {
+  document.querySelector('.modal').style.display = 'none';
 });
